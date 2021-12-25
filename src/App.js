d5001c565117907561417a23d3f3dd3c4a3d8ada
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-25 00:45:07 -0500

    app component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Donnie's Crypto Web</h1>
+    </div>
+  );
+};
+
+export default App;
