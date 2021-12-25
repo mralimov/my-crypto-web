64db113d6a7edf7e9a8d0dbfb9d5b466fb9020a4
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-25 11:01:15 -0500

    setup App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Space, Layout, Typography } from 'antd';
+import { Navbar, Main, Footer } from './components';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Donnie's Crypto Web</h1>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar></Navbar>
+      </div>
+      <div className='main'>
+        <Main></Main>
+      </div>
+      <div className='footer'>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
