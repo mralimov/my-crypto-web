e5b27ef457f6349d38be8a927c188d6c1423ee37
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-26 13:34:21 -0500

    import new Component and setup routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.scss';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Space, Layout, Typography } from 'antd';
-import { Navbar, Main, Footer } from './components';
+import 'antd/dist/antd.css';
+import {
+  Navbar,
+  Main,
+  Footer,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from './components';
 
 const App = () => {
   return (
@@ -11,10 +21,53 @@ const App = () => {
         <Navbar></Navbar>
       </div>
       <div className='main'>
-        <Main></Main>
+        <Main>
+          <Layout>
+            <div className='routes'>
+              <Switch>
+                <Route exact path='/'>
+                  <Homepage />
+                </Route>
+
+                <Route exact path='/exchanges'>
+                  <Exchanges />
+                </Route>
+
+                <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies />
+                </Route>
+
+                <Route exact path='/crypto/:coinid'>
+                  <CryptoDetails />
+                </Route>
+
+                <Route exact path='/news'>
+                  <News />
+                </Route>
+              </Switch>
+            </div>
+          </Layout>
+        </Main>
       </div>
-      <div className='footer'>
-        <Footer></Footer>
+      <div
+        className='footer'
+        level={5}
+        style={{ color: 'white', textAlign: 'center' }}
+      >
+        <Typography.Title>
+          Cryptoverse <br /> All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link exact to='/'>
+            Home
+          </Link>
+          <Link exact to='/Exhanges'>
+            Exchanges
+          </Link>
+          <Link exact to='/News'>
+            News
+          </Link>
+        </Space>
       </div>
     </div>
   );
