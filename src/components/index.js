81837094e01ed230a9f7d84a3650766e21e1f786
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-12-26 13:34:41 -0500

    export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Main } from './main/Main';
 export { default as Footer } from './footer/Footer';
+export { default as Cryptocurrencies } from './Cryptocurrencies.jsx';
+export { default as Exchanges } from './Exchanges.jsx';
+export { default as CryptoDetails } from './CryptoDetails.jsx';
+export { default as Homepage } from './Homepage.jsx';
+export { default as News } from './News.jsx';
